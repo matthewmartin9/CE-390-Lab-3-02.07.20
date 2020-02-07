14e0da615a34d01e900fd51db2cee2ddec2c2d14
Matthew Martin <mmartin9@stevens.edu>
2020-02-07 10:16:14 -0500

    1

--- LAB3.ino
+++ LAB3.ino
@@ -1,10 +1,21 @@
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+
+
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000); 
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  digitalWrite(7, HIGH);
+  delay(100);
+  digitalWrite(7, LOW);
+  digitalWrite(6, HIGH);
+  delay(100);
+  digitalWrite(6, LOW);
+  digitalWrite(5, HIGH);
+  delay(100);
+  digitalWrite(5, LOW);
+  
 }
